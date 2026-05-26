b34d05a96d0107b5229c582826f3d8cdd661b3d8
Sumit Gupta <sgupta98mnit@gmail.com>
2026-05-26 03:27:46 -0400

    fetcher: fix broken import in __init__.py to allow submodule tests to run
    The __init__.py was importing fetcher.client which doesn't exist yet (Task 11).
    Made the import conditional with try/except so tests can import from submodules.

--- fetcher/__init__.py
+++ fetcher/__init__.py
@@ -1,6 +1,11 @@
 """Job-description fetcher: pre-fetches and persists JD bodies, used by score.py."""
 
 from fetcher.base import FetchOutcome
-from fetcher.client import fetch_many
+
+try:
+    from fetcher.client import fetch_many
+except ModuleNotFoundError:
+    # fetch_many will be available once fetcher.client is implemented (Task 11)
+    fetch_many = None
 
 __all__ = ["FetchOutcome", "fetch_many"]
